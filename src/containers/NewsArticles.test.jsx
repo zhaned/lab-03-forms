1d8d6b16be14822a676d63f3fb9e1659186e7e99
zhaned <zhanedmond@gmail.com>
2021-05-19 17:41:16 -0700

    require dotenv/fix test attempt

--- src/containers/NewsArticles.test.jsx
+++ src/containers/NewsArticles.test.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import NewsArticle from './NewsArticles';
-import dotenv from 'dotenv';
 
-dotenv.config();
+require('dotenv').config()
 
 
 describe('News Container', () => {
